8bda619c892929d2e42e7d430291600f8fb780c3
erverted <ervinmitra@gmail.com>
2023-03-08 16:46:27 -0600

    add controller getAlbums

--- gin-service/main.go
+++ gin-service/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // album represents data about a record album
 type album struct {
 	ID     string  `json:"id"`
@@ -12,4 +18,16 @@ type album struct {
 var albums = []album{
 	{ID: "1", Title: "Fractals", Artist: "Subtronics", Price: 6.99},
 	{ID: "2", Title: "Pay Attention EP", Artist: "um..", Price: 20.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
