ab484090f31f7a1af40b447958b40e84f1b22716
erverted <ervinmitra@gmail.com>
2023-03-10 17:58:13 -0600

    add POST route

--- gin-service/main.go
+++ gin-service/main.go
@@ -25,6 +25,7 @@ func main() {
 
 	// router
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -33,4 +34,18 @@ func main() {
 // AKA handler/controller
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
