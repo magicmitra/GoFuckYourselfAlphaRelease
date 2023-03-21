205bf8f9d129eec03f81eff47026fa99ac89a791
erverted <ervinmitra@gmail.com>
2023-03-21 00:47:10 -0500

    call float64 and int64 maps

--- basic-bitches/main.go
+++ basic-bitches/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func main() {
+	// Initialize a map for integer vals
+	ints := map[string]int64{
+		"first":  69,
+		"second": 77,
+	}
+
+	// Initialize a map for float vals
+	floats := map[string]float64{
+		"first":  69.99,
+		"second": 77.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+}
+
 // SumInts adds together the values of m
 func SumInts(m map[string]int64) int64 {
 	var accumulator int64
