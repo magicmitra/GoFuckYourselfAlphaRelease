d51382c44ccd9f4cc629b8c9b4b9584adfb96f72
erverted <ervinmitra@gmail.com>
2023-03-22 17:26:48 -0500

    use type constraint for generic function

--- basic-bitches/main.go
+++ basic-bitches/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	// Initialize a map for integer vals
 	ints := map[string]int64{
@@ -18,6 +22,21 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints),
 		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	// Type arguments can be omitted in calling code when the compiler can
+	// infer the types you want to use. The compiler infers the types from
+	// the types of function arguments passed on to it.
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums, with constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
 
 // SumInts adds together the values of m
@@ -32,6 +51,27 @@ func SumInts(m map[string]int64) int64 {
 // SumFloats adds together the values of m
 func SumFloats(m map[string]float64) float64 {
 	var accumulator float64
+	for _, v := range m {
+		accumulator += v
+	}
+	return accumulator
+}
+
+// SumIntsOrFloats sums up the value of map m.
+// Supports both int64 and float 64
+// Generic function format func name [type parameters] (ordinary function parameters) return type {}
+// int64 | float64 means the union of those types means that this constraint allows either types
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var accumulator V
+	for _, v := range m {
+		accumulator += v
+	}
+	return accumulator
+}
+
+// SumNumbers same as SumIntsOrFloats but uses the Number interface as a type constraint
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var accumulator V
 	for _, v := range m {
 		accumulator += v
 	}
